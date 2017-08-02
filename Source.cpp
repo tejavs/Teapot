15e514480f86fe2bf4672a8151c58cfe4048bcda
tejavs <saitejavenneti@gmail.com>
2017-08-02 11:42:10 +0530

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,16 @@
+#include<iostream>
 #include <GL\glut.h>
+using namespace std;
 #define RED 1
 #define BLUE 2
 #define GREEN 3
 #define ORTHO 4
 
 GLfloat xRotated, yRotated, zRotated;
-GLdouble size = 1;
+GLdouble size1 = 1;
 int mainwindow, subwindow;
+float Cx = -1;
+float Cy = -1;
 
 void display()
 {
@@ -16,22 +20,26 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 	// clear the identity matrix.
 	glLoadIdentity();
+	gluLookAt(0, 0, 0,
+		Cx, Cy, -4.8,
+		0, 1, 0);
 	// traslate the draw by z = -4.0
 	// Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
-	glTranslatef(-0.9, 0.2, -4.5);
+	
+	glTranslatef(-1.0, -1.0, -4.8);
 	// Red color used to draw.
 	//glColor3f(0.8, 0.5, 0.1);
 	// changing in transformation matrix.
 	// rotation about X axis
-	glRotatef(xRotated, 1, 1.0, 0.0);
+	glRotatef(xRotated, 0, 0.0, 0.0);
 	// rotation about Y axis
-	glRotatef(yRotated, 0.0, 1.0, 0.0);
+	glRotatef(yRotated, 0.0, 0.0, 0.0);
 	// rotation about Z axis
 	glRotatef(zRotated, 0.0, 0.0, 0.0);
 	// scaling transfomation 
 	glScalef(0.9, 0.9, 0.9);
 	// built-in (glut library) function , draw you a Teapot.
-	glutSolidTeapot(size);
+	glutSolidTeapot(size1);
 	// Flush buffers to screen
 	
 	glFlush();
@@ -41,15 +49,15 @@ void display()
 
 }
 
-void renderScenesw1()
+/*void renderScenesw1()
 {
 	glutSetWindow(subwindow);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	/*gluLookAt(0, -2, 0,
-		0, 0, 0,
-		-0.9, -0.7, -4.5);*/
-}
+	gluLookAt(-1, -1, Cz,
+		-1.0, -1.0, -4.8,
+		0, 1, 0);
+}*/
 
 void reshapeFunc(int x, int y)
 {
@@ -86,12 +94,55 @@ void processMenuEvents(int option)
 		
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(-2, 4, -1, 2, 1, 50);
+		glOrtho(-1, 1, -1, 1, 3, 50);
 		break;
 	
 	}
 
 }
+
+void processSpecialKeys(int key, int xx, int yy) {
+
+	float fraction = 0.5f;
+
+
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		Cx += fraction;
+		break;
+	case GLUT_KEY_RIGHT:
+		Cx -= fraction;
+		break;
+	case GLUT_KEY_UP:
+		Cy += fraction;
+		break;
+	case GLUT_KEY_DOWN:
+		Cy -= fraction;
+		break;
+		/*angle -= 0.01f;
+		lx = sin(angle);
+		lz = -cos(angle);
+		break;
+	case GLUT_KEY_RIGHT:
+		angle += 0.01f;
+		lx = sin(angle);
+		lz = -cos(angle);
+		break;
+	case GLUT_KEY_UP:
+		x += lx * fraction;
+		z += lz * fraction;
+		break;
+	case GLUT_KEY_DOWN:
+		x -= lx * fraction;
+		z -= lz * fraction;
+		break;*/
+		
+	}
+	
+}
+
+
+
 int createGLUTMenus()
 {
 	int menu;
@@ -112,7 +163,7 @@ int createGLUTMenus()
 
 void idleFunc(void)
 {
-	xRotated += 0.03;
+	xRotated += 0.0;
 	display();
 }
 
@@ -131,6 +182,7 @@ int main(int argc, char **argv)
 	glutReshapeFunc(reshapeFunc);
 	glutIdleFunc(idleFunc);
 
+	glutSpecialFunc(processSpecialKeys);
 	
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	xRotated = yRotated = zRotated = 30.0;
